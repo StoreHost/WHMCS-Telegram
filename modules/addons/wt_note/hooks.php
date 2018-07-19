02de87e7e4123e3ce34bb5c108dcf504ce498268
Dream <sb@store-host.com>
2018-07-19 16:32:08 +0200

    initial commit

---
+++ modules/addons/wt_note/hooks.php
@@ -0,0 +1,59 @@
+<?php
+function sendTelegramMessage($pm) {
+	global $vars;
+	$application_chatid = mysql_fetch_array( select_query('tbladdonmodules', 'value', array('module' => 'wt_note', 'setting' => 'chatid') ), MYSQL_ASSOC );
+	$application_botkey = mysql_fetch_array( select_query('tbladdonmodules', 'value', array('module' => 'wt_note', 'setting' => 'key') ), MYSQL_ASSOC );
+	$chat_id 		= $application_chatid['value'];
+	$botToken 		= $application_botkey['value'];
+
+	$data = array(
+		'chat_id' 	=> $chat_id,
+		'text' 		=> $pm . "\n\n----------------------------------------------------------------------------------------------\n" . base64_decode("V0hNQ1MgVGVsZWdyYW0gaG9vayB2b24gU3RvcmVIb3N0==")
+	);
+
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, "https://api.telegram.org/bot$botToken/sendMessage");
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_exec($curl);
+	curl_close($curl);
+}
+
+function wt_note_ClientAdd($vars) {
+	global $customadminpath, $CONFIG;
+	sendTelegramMessage("Ein neuer Nutzer hat sich Registriert. \n---------------------------------------------------------------------------------------------- \n\n". $CONFIG['SystemURL'].'/'.$customadminpath.'/clientssummary.php?userid='.$vars['userid']);
+}
+
+function wt_note_InvoicePaid($vars) {
+	global $customadminpath, $CONFIG;
+	sendTelegramMessage("Eine Rechnung mit folgenden Angaben wurde bezahlt \n---------------------------------------------------------------------------------------------- \n\n Rechnungs-Nr. : $vars[invoiceid] \n\n Betrag : $vars[total] \n\n". $CONFIG['SystemURL'].'/'.$customadminpath.'/invoices.php?action=edit&id='.$vars['invoiceid']);
+}
+
+function wt_note_TicketOpen($vars) {
+	global $customadminpath, $CONFIG;
+	sendTelegramMessage("Ein neues Ticket wurde erstellt \n---------------------------------------------------------------------------------------------- \n\n Ticket-ID : $vars[ticketid] \n\n Betreff : $vars[deptname] \n\n Betreff : $vars[subject] \n\n". $CONFIG['SystemURL'].'/'.$customadminpath.'/supporttickets.php?action=viewticket&id='.$vars['ticketid']);
+}
+
+function wt_note_TicketUserReply($vars) {
+	global $customadminpath, $CONFIG;
+	sendTelegramMessage("Eine neue Antwort wurde gesendet \n---------------------------------------------------------------------------------------------- \n\n Ticket-ID : $vars[ticketid] \n\n Abteilung : $vars[deptname] \n\n Betreff : $vars[subject] \n\n". $CONFIG['SystemURL'].'/'.$customadminpath.'/supporttickets.php?action=viewticket&id='.$vars['ticketid'], $application_botkey, $application_chatid);
+
+}
+
+#####
+
+function wt_note_FileDownloaded($vars) {
+	global $customadminpath, $CONFIG;
+	sendTelegramMessage("Es wurde eine Datei runtergeladen \n---------------------------------------------------------------------------------------------- \n\n Dateiname : $vars[filename] \n\n SeitenURL : $vars[pagetitle] \n\n". $CONFIG['SystemURL'].'/'.$customadminpath.'/clientssummary.php?userid='.$vars['userid']);
+}
+
+
+add_hook("ClientAdd",1,"wt_note_ClientAdd");
+add_hook("InvoicePaid",1,"wt_note_InvoicePaid");
+add_hook("TicketOpen",1,"wt_note_TicketOpen");
+add_hook("TicketUserReply",1,"wt_note_TicketUserReply");
+
+####
+add_hook("FileDownload",1,"wt_note_FileDownloaded");
---
+++ modules/addons/wt_note/wt_note.php
@@ -0,0 +1,85 @@
+<?php
+if (!defined("WHMCS"))
+	die("Diese Datei kann nicht direkt aufgerufen werden.");
+
+function wt_note_config() {
+	$configarray = array(
+	"name" => "Telegrammbenachrichtigungsmodul",
+	"description" => "Benachrichtigung an den Administrator und die Sponsoren der Website per Telegramm - Weiterentwickelt von StoreHost",
+	"version" => "1.0",
+	"author" => "<a href='http://www.store-host.com' target='_blank' style='color:#0000FF; text-decoration: none;'>StoreHost</a>",
+	"language" => "english",
+	"fields" => array(
+	"key" => array ("FriendlyName" => "Bot Token", "Type" => "text", "Size" => "50", "Description" => "Token von BOTFATHER", "Default" => "", ),
+	"chatid" => array ("FriendlyName" => "Chat ID", "Type" => "text", "Size" => "50", "Description" => "Chat ID", "Default" => "", ),
+	));
+	return $configarray;
+}
+
+function wt_note_activate() {
+	$query = "CREATE TABLE IF NOT EXISTS `wikitelegramnote` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`adminid` int(11) NOT NULL,
+	`access_token` varchar(255) NOT NULL,
+	`permissions` text NOT NULL,
+	PRIMARY KEY (`id`)
+	) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;";
+	$result = mysql_query($query);
+}
+
+function wt_note_deactivate() {
+	$query = "DROP TABLE `wikitelegramnote`";
+	$result = mysql_query($query);
+}
+
+function wt_note_output($vars) {
+	global $customadminpath, $CONFIG;
+
+	$access_token = select_query('wikitelegramnote', '', array('adminid' => $_SESSION['adminid']));
+
+	if ( $_GET['return'] == '1' && $_SESSION['request_token'] ) {
+		
+		insert_query("wikitelegramnote", array("adminid" => $_SESSION['adminid'], "access_token" => $result['access_token']));
+		$_SESSION['request_token'] = "";
+		header("Location: addonmodules.php?module=wt_note");
+		
+	} elseif($_GET['setup'] == '1' && !mysql_num_rows($access_token)) {
+
+		$_SESSION['request_token'] = $vars['key'];
+		header("Location: ". $CONFIG['SystemURL']."/".$customadminpath."/addonmodules.php?module=wt_note&return=1");
+
+	} elseif( $_GET['disable'] == '1' && mysql_num_rows($access_token) ) {
+		full_query("DELETE FROM `wikitelegramnote` WHERE `adminid` = '".$_SESSION['adminid']."'");
+		echo "<div class='infobox'><strong>Das Benachrichtigungs-Plugin wurde erfolgreich deaktiviert</strong><br>Die Datenbank des Benachrichtigungs-Plugins wurde erfolgreich gelöscht und das Plugin wurde deaktiviert</div>";
+	} elseif( mysql_num_rows($access_token) && $_POST ){
+		update_query('wikitelegramnote',array('permissions' => serialize($_POST['wt_notefication'])), array('adminid' => $_SESSION['adminid']));
+		echo "<div class='infobox'><strong>Änderungen gespeichert</strong><br>Änderungen erfolgreich gespeichert</div>";    
+	}
+
+	$access_token = select_query('wikitelegramnote', '', array('adminid' => $_SESSION['adminid']));
+	$result = mysql_fetch_array($access_token, MYSQL_ASSOC);
+	$permissions = unserialize($result['permissions']);   
+
+	if ( !mysql_num_rows($access_token)) {
+		echo "<p><a href='addonmodules.php?module=wt_note&setup=1'>Aktivieren Sie das System, um Benachrichtigungen zu senden</a></p>";
+	} else {
+		echo "<p><a href='addonmodules.php?module=wt_note&disable=1'>Benachrichtigungssystem deaktivieren</a></p>";
+		echo '<form method="POST"><table class="form" width="100%" border="0" cellspacing="2" cellpadding="3">
+		<tr>
+		<td class="fieldlabel" width="200px">Nachricht senden wenn :</td>
+		<td class="fieldarea">
+		<table width="100%">
+		<tr>
+		<td valign="top">
+		<input type="checkbox" name="wt_notefication[new_client]" value="1" id="wt_notefications_new_client" '.($permissions['new_client'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_client">Neuer Benutzer registriert</label><br>
+		<input type="checkbox" name="wt_notefication[new_invoice]" value="1" id="wt_notefications_new_invoice" '.($permissions['new_invoice'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_invoice">Rechnung Bezahlt</label><br>
+		<input type="checkbox" name="wt_notefication[new_update]" value="1" id="wt_notefications_new_update" '.($permissions['new_update'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_update">Antwort auf Ticket</label><br>
+		<input type="checkbox" name="wt_notefication[new_ticket]" value="1" id="wt_notefications_new_ticket" '.($permissions['new_ticket'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_ticket">Ticket erstellt wurde</label><br>
+		<input type="checkbox" name="wt_notefication[new_download]" value="1" id="wt_notefications_new_download" '.($permissions['new_download'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_download">Eine Datei runtergeladen wurde.</label><br>
+		</td>
+		</tr>
+		</table>
+		</table>
+		<p align="center"><input type="submit" value="Änderungen speichern" class="button"></p></form>';
+	}
+}
